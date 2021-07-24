3a62305d284554077d07a90a5a3e3d67a20a1aee
wildlyclassyprince <lihtumb@gmail.com>
2021-07-24 23:50:21 +0400

    Update init() to upload mockDoc

--- documentRegistryCC/lib/documentRegistry.js
+++ documentRegistryCC/lib/documentRegistry.js
@@ -1,21 +1,36 @@
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
-const docID = "document-1";
-const docType = "Certificate";
-const school = "Hard Knocks";
-const studentName = "Bell Bell";
-const studentGrade = "Amateur";
-const studentScore = "Passed";
 
 class documentRegistryContract extends Contract {
     // Initialize
-    async init(ctx, docID, docType, school, studentName, studentGrade, studentScore) {
+    async init(ctx) {
         
         if (!ctx.clientIdentity.assertAttributeValue("init", "true")) {
             throw new Error(`you do not have permissions to initialize an account`)
         }
 
+        // Initialize with mock entry
+        const mockDoc = {
+            id: "mock-document",
+            type: "bachelors",
+            school: "Cornell",
+            name: "Andy",
+            grade: "Merit",
+        }
+
+        // Check if document exists
+        if (await this._docExists(ctx, mockDoc.id)) {
+            throw new Error(`document ${mockDoc.id} already exists`)
+        }
+
+        // Add document
+        await this._putDoc(ctx, mockDoc);
+        console.log(`Successfully entered ${mockDoc.id}`)
+
+    };
+    // Add document to registry
+    async uploadDoc(ctx, docID, docType, school, studentName, studentGrade, studentScore){
         const document = {
             id: docID,
             type: docType,
@@ -24,19 +39,6 @@ class documentRegistryContract extends Contract {
             grade: studentGrade,
             score: studentScore,
         }
-
-        // Check if document exists
-        if (await this._docExists(ctx, document.id)) {
-            throw new Error(`document ${document.id} already exists`)
-        }
-
-        // Add document
-        await this._putDoc(ctx, document);
-        console.log(`Successfully entered ${document.id}`)
-
-    };
-    // Add document to registry
-    async uploadDoc(ctx, docID, docType, school, studentName, studentGrade, studentScore){    
     };
     async getDoc(){}
     async delDoc(){}
