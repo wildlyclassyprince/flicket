6784b9225060bfd1e2a961c1122a2ce361c2707d
wildlyclassyprince <lihtumb@gmail.com>
2021-07-24 22:20:31 +0400

    Initial commit

---
+++ documentRegistryCC/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const documentRegistryContract = require('./lib/documentRegistryContract');
+
+module.exports.DocumentRegistryContract = documentRegistryContract;
+module.exports.contracts = [documentRegistryContract];
