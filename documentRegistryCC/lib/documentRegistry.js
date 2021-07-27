7c5101f7944a0bb84e5211b7592115c63ec591ef
wildlyclassyprince <lihtumb@gmail.com>
2021-07-27 22:00:52 +0400

    Re-write smart contract - initial commit

--- documentRegistryCC/lib/documentRegistry.js
+++ documentRegistryCC/lib/documentRegistry.js
@@ -2,6 +2,10 @@
 
 const { Contract } = require('fabric-contract-api');
 
-class documentRegistryContract extends Contract {}
+class documentRegistryContract extends Contract {
+    async uploadDoc(){};
+    async deleteDoc(){};
+    async readDoc(){};
+};
 
 module.exports = documentRegistryContract;
