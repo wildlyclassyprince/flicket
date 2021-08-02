ac180c10cb91a4d8d5ecbb2e7ffddf2edc2bbd26
wildlyclassyprince <lihtumb@gmail.com>
2021-08-02 04:21:42 +0400

    Delete files

--- documentRegistryCC/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const documentRegistryContract = require('./lib/documentRegistryContract');
-
-module.exports.DocumentRegistryContract = documentRegistryContract;
-module.exports.contracts = [documentRegistryContract];
