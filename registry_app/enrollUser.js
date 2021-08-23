2d5ba2a5116499eb4a8455156139902b135809c9
wildlyclassyprince <wildlyclassyprince@users.noreply.github.com>
2021-08-23 22:27:55 +0400

    Read Fabric CA server URL

--- registry_app/enrollUser.js
+++ registry_app/enrollUser.js
@@ -8,7 +8,19 @@ const { Wallets } = require('fabric-network');
 
 async function main() {
     try {
+        // Get Fabric CA server URL - already defined in `test-network` connection profile
+        let args = process.argv.slice(2);
+
+        const identityLabel = args[0];
+        const orgName = identityLabel.split('@')[1];
+        const orgNameWithoutDomain = orgName.split('.')[0];
+
+        let connectionProfile = JSON.parse(fs.readFileSync(path.join(testNetworkRoot, 'organizations/peerOrganizations', orgName, `/connection-${orgNameWithoutDomain}.json`), 'utf-8'));
+
+        const ca = new FabricCAServices(connectionProfile['certificateAuthorities'][`ca.${orgName}`].url);
+
         // User enrollment
+
     } catch (error) {
         console.error(`Failed to enroll user: ${error}`);
         process.exit(1);
