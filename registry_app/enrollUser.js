c1c9782d686bea97ca51a9f66b67aa509e868af4
wildlyclassyprince <wildlyclassyprince@users.noreply.github.com>
2021-08-23 22:16:43 +0400

    Initial commit

---
+++ registry_app/enrollUser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const FabricCAServices = require('fabric-ca-client');
+const { Wallets } = require('fabric-network');
+
+async function main() {}
