d53dba1a4403355c66afe2aa7bba89b49bca215b
wildlyclassyprince <wildlyclassyprince@users.noreply.github.com>
2021-08-23 22:20:37 +0400

    Add error catching

--- registry_app/enrollUser.js
+++ registry_app/enrollUser.js
@@ -6,4 +6,20 @@ const path = require('path');
 const FabricCAServices = require('fabric-ca-client');
 const { Wallets } = require('fabric-network');
 
-async function main() {}
+async function main() {
+    try {
+        // User enrollment
+    } catch (error) {
+        console.error(`Failed to enroll user: ${error}`);
+        process.exit(1);
+    }
+}
+
+main().then(() => {
+    console.log('User enrollment completed successfully');
+}).catch((e) => {
+    console.log('User enrollment exception');
+    console.log(e);
+    console.log(e.stack);
+    process.exit(-1);
+});
