99fcb7c45f2806403b81b65cf8d19913ce00219d
wildlyclassyprince <lihtumb@gmail.com>
2021-08-30 03:22:03 +0400

    Update submitTransaction to use registry_chaincode

--- registry_app/submitTransaction.js
+++ registry_app/submitTransaction.js
@@ -1,9 +1,59 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 const { Wallets, Gateway } = require('fabric-network');
-const _ = require('lodash');
+
+const testNetworkRoot = path.resolve(require('os').homedir(), 'go/src/github.com/hyperledger/fabric-samples/test-network');
 
 async function main() {
     const gateway = new Gateway();
     const wallet = await Wallets.newFileSystemWallet('./wallet');
+
+    try {
+        let args = process.argv.slice(2);
+
+        const identityLabel = args[0];
+        const functionName = args[1];
+        const chaincodeArgs = args.slice(2);
+
+        const orgName = identityLabel.split('@')[1];
+        const orgNameWithoutDomain = orgName.split('.')[0];
+
+        let connectionProfile = JSON.parse(fs.readFileSync(
+            path.join(testNetworkRoot,
+                'organizations/peerOrganizations',
+                orgName,
+                `/connection-${orgNameWithoutDomain}.json`), 'utf-8'));
+
+        let connnectionOptions = {
+            identity: identityLabel,
+            wallet: wallet,
+            discovery: {enables: true, asLocalhost: true}
+        };
+
+        console.log('Connect to a Hyperledger Fabric gateway');
+        await gateway.connect(connectionProfile, connectionOptions);
+
+        console.log('Use channel "mychannel".');
+        const network = await gateway.getNetwork('mychannel');
+
+        console.log('Use Registry Contract.');
+        const contract = network.getContract('registry_chaincode');
+
+        console.log('Submit ' + functionName + ' transaction.');
+        const response = await contract.submitTransaction(functionName, ...chaincodeArgs);
+        if (`${response}` !== '') {
+            console.log(`Response from ${functionName}: ${response}`);
+        }
+    } catch (error) {
+        console.log(`Error processing transaction. ${error}`);
+        console.log(error.stack);
+    } finally {
+        console.log('Disconnect from the gateway.');
+        gateway.disconnect();
+    }
 }
+
+main();
